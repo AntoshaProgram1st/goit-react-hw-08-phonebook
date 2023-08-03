ad6f54bb18e1047bc8fe0efde2f721bb513fb54f
unknown <antoxa1012@gmail.com>
2023-08-03 21:21:05 +0300

    15

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import AppBar from 'components/appBar'
 import React, { Suspense } from 'react'
 import { Toaster } from 'react-hot-toast'
-import { Outlet } from ' '
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
     return (
